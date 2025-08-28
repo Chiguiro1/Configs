981e404622fe10c196e4d12172c3b7c0b60f1d3e
chiguiro <jimenezmjuanesteban@gmail.com>
2025-08-27 21:26:59 -0500

    1.4 estable

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,7 +1,9 @@
 local lspconfig = require("lspconfig")
 local cmp = require("cmp")
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-require("cmp").setup({
+-- Autocompletado con nvim-cmp
+cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ["<Tab>"] = cmp.mapping.select_next_item(),
     ["<S-Tab>"] = cmp.mapping.select_prev_item(),
@@ -12,10 +14,38 @@ require("cmp").setup({
   },
 })
 
--- Habilita LSPs
-lspconfig.lua_ls.setup({})
-lspconfig.pyright.setup({})
-lspconfig.tsserver = nil
-lspconfig.ts_ls.setup({})
-lspconfig.phpactor.setup({})
+-- Capabilities para LSP + cmp
+local capabilities = cmp_nvim_lsp.default_capabilities()
 
+-- Función auxiliar para configurar todos los LSPs igual
+local function setup_server(server, opts)
+  opts = opts or {}
+  opts.capabilities = capabilities
+  lspconfig[server].setup(opts)
+end
+
+-- Lista de servidores a configurar
+local servers = {
+  lua_ls = {
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { "vim" },
+        },
+      },
+    },
+  },
+  pyright = {},
+  ts_ls= {},
+  phpactor = {},
+  clangd = {},
+  bashls = {},
+  jsonls = {},
+  html = {},
+  cssls = {},
+}
+
+-- Configurar todos los LSPs
+for server, opts in pairs(servers) do
+  setup_server(server, opts)
+end
